5a8d852583f0ba1290a0760c1bd267bd9cf67906
rahulunair <rahulunair@gmail.com>
2014-12-13 00:57:46 -0600

    strramlined db classes added methods

--- mongo_pipline.py
+++ mongo_pipline.py
@@ -1,11 +1,7 @@
-#==============
-#mongodb
-#db.eia_data.find("name": {$regex: "/Net Generation : .*?: .*?: All Sectors : Annual/i"})
-#==============
-import pymongo as pm
 import os
 import collections
-
+import re
+from MongoConnect import MongoConnect
 #constants
 
 field = 'name'
@@ -17,15 +13,12 @@ query_2 = 'Net Generation : (.*?) : .*? : All Sectors : Annual'
 q1_list = list()
 q2_list = list()
 uniq_dict = collections.defaultdict(str)
+mongo = MongoConnect()
 
-try:
-	conn = pm.MongoClient()
-	print 'Connected to Mongo Db: ', conn
-	db = conn.energy_data
-	print 'connected to database: ',db
 
-except pm.errors.ConnectionFailure, e:
-	print ' Cannot connect to database', e
+# connecting to database
+mc = mongo.conn() 
+
 
 def test_list(q_list):
 	for i in q_list:
@@ -38,7 +31,7 @@ def find_row(field, query):
 	'''
 	# creating views
 	raw_data = []
-	for row in db.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0}):
+	for row in mc.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0}):
 		raw_data.append(row)
 	return raw_data
 
@@ -47,33 +40,31 @@ def find_name (field, query):
 	A view to return all names in the dataset that match the regex query
 	'''
 	names = []
-	for name in db.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0, field : 1}):
+	for name in mc.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0, field : 1}):
 		names.append(name['name'])
 	return names	
 
 def find_uniq_descriptions (field, query):
+	'''
+	A view to list all energy sources with their descriptions
+	'''
 	uniq_data = collections.defaultdict(str)
 	for r in find_row(field, query):
-		name = r['name']
-		uniq_data[name] = r
+		if r['name'].split(':')[1] not in uniq_data:
+			uniq_data[r['name'].split(':')[1]] = r['description']
 	return uniq_data
-
-
-
 
 
 # testing modules
 
-#q1_list = find_row(field, query)
-#q2_list = find_name(field, query)
-q3_list =  find_uniq_descriptions(field, query_2)
+q1_list = find_row(field, query_1)
+q2_list = find_name(field, query_1)
+e_types = find_uniq_descriptions(field, query_2)
 
 
 #test_list(q1_list)
-#test_list(q2_list)
-for k, v in q3_list.items():
-	if not k:
-		print k
-		key -= k
+test_list(q2_list)
 
+ # closing db
+mongo.close_con()
 
