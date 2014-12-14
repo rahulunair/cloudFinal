5251132256821c47b8cb29ea6b5758e03daaa21b
rahulunair <rahulunair@gmail.com>
2014-12-14 12:22:51 -0600

    modified MongoConnect.py to add seds table, created views for accessing the same in mongo_pipile.py file

--- MongoConnect.py
+++ MongoConnect.py
@@ -1,4 +1,19 @@
 import pymongo as pm
+'''
+Before using this class please import all data sources into the mongodb using the command:
+
+"mongoimport --db <db-name> --collection <coll-name> --type json --file seed.json --jsonArray"
+
+If mongodb has been configured correctly, then it should take a second to write
+about 1 million rows in fire and forget mode. After which using views from mongo_pipeline can
+access data in the file.
+
+Be sure to close the database once the use is complete, not in between, a connection pool
+is available to mongo client which will take care of connections during the course of the program.
+
+Presently, the db is connected to localhost at 27017. As this is  a nosql database, which can be distributed
+accross nodes, it would be seemless to extend the functionality.
+'''
 
 
 class MongoConnect:
@@ -9,13 +24,28 @@ class MongoConnect:
 		try:
 			self.con = pm.MongoClient()
 			print 'Connected to Mongo Db: ', self.con
-			self.db = self.con.energy_data
-			print 'connected to database: ',self.db
 		except pm.errors.ConnectionFailure, e:
 			print ' Cannot connect to database', e
 
-	def conn(self):
+	def energy_data (self):
+		'''
+		energy databasereturn a table handle
+		'''
+		self.db = self.con.energy_data
+		print 'connected to database: ',self.db
 		return self.db
 
+	def seds_table (self):
+		'''
+		seds databasereturns a table handle
+		'''
+		self.db = self.con.seds_data
+		print 'connected to seds database: ', self.db
+		return self.db
+
+
 	def close_con(self):
-		self.con.close()			
+		'''
+		close connection to the database
+		'''
+		self.con.close()
--- mongo_pipline.py
+++ mongo_pipline.py
@@ -4,9 +4,14 @@ import re
 from MongoConnect import MongoConnect
 #constants
 
-field = 'name'
-query_1 = 'Net Generation : .*?: .*?: All Sectors : Annual'
-query_2 = 'Net Generation : (.*?) : .*? : All Sectors : Annual'
+field   = 'name'
+field_1 = 'units' 
+
+e_query_1 = 'Net Generation : .*?: .*?: All Sectors : Annual'
+e_query_2 = 'Net Generation : (.*?) : .*? : All Sectors : Annual'
+
+s_query_1 = 'Electricity .*? the United States, .*?'  # data
+s_query_2 =  'Billion Btu' #units
 
 # global variables
 
@@ -16,8 +21,12 @@ uniq_dict = collections.defaultdict(str)
 mongo = MongoConnect()
 
 
-# connecting to database
-mc = mongo.conn() 
+# connecting to energy_database 
+e_mongo = mongo.energy_data() # includes eia and seds table
+
+# connecting to seds database
+#s_mongo = mongo.seds_data()
+
 
 
 def test_list(q_list):
@@ -25,26 +34,26 @@ def test_list(q_list):
 		print i, '\n'
 
 
-def find_row(field, query): 
+def find_row(field, query):  # eia_data
 	'''
 	A view to return all rows in the dataset whose 'name' field matches the regex 'query'
 	'''
 	# creating views
 	raw_data = []
-	for row in mc.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0}):
+	for row in e_mongo.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0}):
 		raw_data.append(row)
 	return raw_data
 
-def find_name (field, query):
+def find_name (field, query):  # eia_data
 	'''
 	A view to return all names in the dataset that match the regex query
 	'''
 	names = []
-	for name in mc.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0, field : 1}):
+	for name in e_mongo.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0, field : 1}):
 		names.append(name['name'])
 	return names	
 
-def find_uniq_descriptions (field, query):
+def find_uniq_descriptions (field, query):  # eia_data
 	'''
 	A view to list all energy sources with their descriptions
 	'''
@@ -55,15 +64,30 @@ def find_uniq_descriptions (field, query):
 	return uniq_data
 
 
-# testing modules
 
-q1_list = find_row(field, query_1)
-q2_list = find_name(field, query_1)
-e_types = find_uniq_descriptions(field, query_2)
+def get_ie(field,field_1, query, query_1):  #seds data	
+	'''
+	A view to get complete data based on name and units
+	'''
+	raw_data = []
+	for row in e_mongo.eia_data.find({field: {'$regex':query, '$options':'$i'}}, {'_id' : 0}) and \
+	e_mongo.eia_data.find({field_1: {'$regex':query_1, '$options':'$i'}}, {'_id' : 0}):
+		raw_data.append(row)
+	return raw_data
+
+
+
+# testing modules 
+
+q1_list = find_row(field, e_query_1)
+q2_list = find_name(field, e_query_1)
+e_types = find_uniq_descriptions(field, e_query_2)
+s_list= get_ie(field, field_1, s_query_1, s_query_2)
 
 
 #test_list(q1_list)
-test_list(q2_list)
+#test_list(q2_list)
+test_list(s_list)
 
  # closing db
 mongo.close_con()
